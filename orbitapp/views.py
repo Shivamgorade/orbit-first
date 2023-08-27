945fe9444d35f76c7480bd3f04ea05ae80030373
shivam gorade <contactshivamgorade@gmail.com>
2023-08-27 18:10:37 +0530

    initial commit

--- orbitapp/views.py
+++ orbitapp/views.py
@@ -4,4 +4,4 @@ from orbitapp.models import Card
 
 def home(request):
     card_data = Card.objects.all()
-    return render(request, 'orbitapp/home.html', {'card_data': card_data})
+    return render(request, 'orbitapp/index.html', {'card_data': card_data})
