99b4bc762ace45cc448fc501c952114f93e23297
shivam gorade <contactshivamgorade@gmail.com>
2023-09-01 00:34:37 +0530

    second commit

--- orbitapp/views.py
+++ orbitapp/views.py
@@ -1,7 +1,14 @@
-# myapp/views.py
 from django.shortcuts import render
-from orbitapp.models import Card
+from django.db.models import Q  # Import the Q object
+from .models import Card
 
 def home(request):
+    query = request.GET.get('q')
     card_data = Card.objects.all()
-    return render(request, 'orbitapp/index.html', {'card_data': card_data})
+
+    if query:
+        card_data = card_data.filter(
+            Q(company_name__icontains=query) | Q(location__icontains=query) | Q(designation__icontains=query)
+        )
+
+    return render(request, 'orbitapp/index.html', {'card_data': card_data, 'query': query})
