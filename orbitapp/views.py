30e4f9fd367775eadd79b737cfb5bffe6f6dd928
shivam gorade <contactshivamgorade@gmail.com>
2023-09-04 00:44:01 +0530

    fourth commit

--- orbitapp/views.py
+++ orbitapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.db.models import Q  # Import the Q object
+from django.db.models import Q
 from .models import Card
 
 def home(request):
+    # Add any data or logic you need for this view
+    return render(request, 'orbitapp/home.html') 
+
+
+def find_job(request):
     query = request.GET.get('q')
     card_data = Card.objects.all()
 
@@ -11,4 +16,4 @@ def home(request):
             Q(company_name__icontains=query) | Q(location__icontains=query) | Q(designation__icontains=query)
         )
 
-    return render(request, 'orbitapp/index.html', {'card_data': card_data, 'query': query})
+    return render(request, 'orbitapp/find_job.html', {'card_data': card_data, 'query': query})
